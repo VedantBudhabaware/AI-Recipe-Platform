a82bccb755822b23aa7785e5a17887ed966c1b77
Vedant <vedantbudhabaware@gmail.com>
2026-01-31 23:01:41 +0530

    02: Add dashboard page

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
+import { neobrutalism } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+        baseTheme: neobrutalism,
+      }}>
       <html lang="en" suppressHydrationWarning>
         <body className={`${inter.className} antialiased`}>
           <Header />
--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 
 
@@ -7,7 +7,12 @@ export default async function Header() {
     return (
         <header className="fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60">
             <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
-
+                Logo
+                <div>Nav Links</div>
+                <div className="flex items-center space-x-4">
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
                     <SignedOut>
                         <SignInButton mode="modal">
                             <Button
@@ -23,6 +28,7 @@ export default async function Header() {
                             </Button>
                         </SignUpButton>
                     </SignedOut>
+                </div>
             </nav>
         </header>
     );
